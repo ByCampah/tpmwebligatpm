343232b745c3a36ee8d59a89a0e1448cc65fbacf
ByCampah <bausdones@gmail.com>
2026-06-23 14:57:02 -0300

    fix: prisma import

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import DiscordProvider from "next-auth/providers/discord"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import prisma from "@/lib/prisma"
+import { prisma } from "@/lib/prisma"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
