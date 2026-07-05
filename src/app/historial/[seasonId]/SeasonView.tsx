6b0ab2c8933e9f8aeae55d58d410d60a4823698f
ByCampah <bausdones@gmail.com>
2026-07-05 07:17:13 -0300

    fix: import BracketViewer in SeasonView

--- src/app/historial/[seasonId]/SeasonView.tsx
+++ src/app/historial/[seasonId]/SeasonView.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import Link from "next/link";
 import { getFlagUrl } from "@/lib/flags";
 import { useSearchParams } from "next/navigation";
+import BracketViewer from "@/components/BracketViewer";
 
 interface TournamentData {
   id: string;
